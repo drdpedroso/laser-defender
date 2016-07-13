efe34bc2ff867cfde1864e5cbb8410dbd142f210
Eduardo Pedroso <drdpedroso@gmail.com>
2016-07-12 22:32:40 -0300

    Adding ship and player controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed = 15.0f;
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}
+	}
+}

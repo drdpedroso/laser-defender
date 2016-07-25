1e131a0bc73224a5a95543f16d6cedcd789040a9
Eduardo Pedroso <drdpedroso@gmail.com>
2016-07-25 17:26:08 -0300

    AddingAdding collision to enemies

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log("Hit by a projectile");
+		}
+		
+	}
+	
+}

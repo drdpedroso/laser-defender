632f7118afcd4673eda33216f6aaef677e168b36
Eduardo Pedroso <drdpedroso@gmail.com>
2016-07-25 19:57:06 -0300

    Killing enemies when hited

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
+
+	public float healt = 150f;
 	
 	void OnTriggerEnter2D(Collider2D collider){
 		Projectile missile = collider.gameObject.GetComponent<Projectile>();
 		if(missile){
-			Debug.Log("Hit by a projectile");
+		    healt -= missile.GetDamage();
+		    missile.Hit();
+		    if(healt <= 0){
+		    	Destroy(gameObject);
+		    }
 		}
 		
 	}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,4 +5,12 @@ public class Projectile : MonoBehaviour {
 	
 	public float damage = 100f;
 	
+	public float GetDamage(){
+		return damage;
+	}
+	
+	public void Hit(){
+		Destroy(gameObject);
+	}
+	
 }

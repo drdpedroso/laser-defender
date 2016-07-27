57971845dbd3d85e6699453b732edea4170ff2de
Eduardo Pedroso <drdpedroso@gmail.com>
2016-07-26 22:55:53 -0300

    UpdaEnemies atacking

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,12 +6,20 @@ public class EnemyBehaviour : MonoBehaviour {
 	public GameObject projectile;
 	public float projectileSpeed = 10f;
 	public float healt = 150f;
-	
+	public float shotsPerSeconds = 0.2f;
 	
 	void Update(){
+		float probability = Time.deltaTime * shotsPerSeconds;
+		if(Random.value < probability){
+			Fire ();
+		}
+	}
+	
+	
+	void Fire(){
 		Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
 		GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
-		missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0,projectileSpeed);
+		missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
 	}
 	
 	void OnTriggerEnter2D(Collider2D collider){
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
 	public GameObject projectile;
 	public float projectileSpeed;
 	public float firingRate = 0.2f;
+	public float healt = 250f;
 	
 	float xmin;
 	float xmax;
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void Fire() {
-		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		Vector3 offset = new Vector3(0, 1, 0);
+		GameObject beam = Instantiate(projectile, transform.position + offset, Quaternion.identity) as GameObject;
 		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
 	}
 	
@@ -44,5 +46,19 @@ public class PlayerController : MonoBehaviour {
 		// restrict the player to the game space
 		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
 		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+	
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log("Player hi");
+			healt -= missile.GetDamage();
+			missile.Hit();
+			if(healt <= 0){
+				Destroy(gameObject);
+			}
+		}
+		
 	}
 }

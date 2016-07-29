e9785c92b6ed0de2a61ef0394cc5f7ea19f1512a
Eduardo Pedroso <drdpedroso@gmail.com>
2016-07-28 21:54:17 -0300

    Anemies respawing one by one

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour {
 	public float width = 10f;
 	public float height = 5f;
 	public float speed = 5;
+	public float spawnDelay = 0.5f;
 	
 	private bool movingRight = true;
 	private float xmax;
@@ -22,14 +23,25 @@ public class EnemySpawner : MonoBehaviour {
 		
 		xmax = rightBoundary.x;
 		xmin = leftBoundary.x;
-		SpawnEnemies();
+		SpawnUntilFull();
 	}
 	
 	void SpawnEnemies(){
-		
 		foreach(Transform child in transform){
 			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
+		}
+	}
+	
+	void SpawnUntilFull(){
+		Transform freePosition = NextFreePosition();
+		if(freePosition){
+			GameObject enemy = Instantiate(enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = freePosition;
+		}
+		
+		if(NextFreePosition()){
+			Invoke("SpawnUntilFull", spawnDelay);	
 		}
 	}
 	
@@ -55,9 +67,18 @@ public class EnemySpawner : MonoBehaviour {
 		
 		if(AllMembersDead()){
 			Debug.Log("Empty Formation");
-			SpawnEnemies();
+			SpawnUntilFull();
 		}
 	
+	}
+	
+	Transform NextFreePosition(){
+		foreach(Transform childPositionGameObject in transform){
+			if(childPositionGameObject.childCount == 0){
+				return childPositionGameObject;
+			}
+		}
+		return null;
 	}
 	
 	bool AllMembersDead(){
